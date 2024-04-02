f4c806d602a2acb794d3cee39dfcbb5581b28f10
CarlosEduardo-IFF <149334691+CarlosEduardo-IFF@users.noreply.github.com>
2024-04-01 22:32:43 -0300

    Update ClienteRepository.java

--- E-commerce/src/main/java/repository/ClienteRepository.java
+++ E-commerce/src/main/java/repository/ClienteRepository.java
@@ -11,25 +11,28 @@ import jakarta.transaction.Transactional;
 @Repository
 public interface ClienteRepository extends JpaRepository<ContaCliente, Long> {
 
-    @Query(value = "SELECT * FROM ContaCliente WHERE email = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_conta_cliente WHERE col_email = ?1", nativeQuery = true)
     ContaCliente buscarPorEmail(String email);
 
-    @Query(value = "SELECT * FROM ContaCliente WHERE cpf = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_conta_cliente WHERE col_cpf = ?1", nativeQuery = true)
     ContaCliente buscarPorCpf(String cpf);
 
     @Transactional
     @Modifying
-    @Query(value = "DELETE FROM ContaCliente WHERE id = ?1", nativeQuery = true)
+    @Query(value = "DELETE FROM tb_conta_cliente WHERE col_id_conta = ?1", nativeQuery = true)
     void removerPorId(Long id);
 
     @Transactional
     @Modifying
-    @Query(value = "UPDATE ContaCliente SET creditosCliente = ?2 WHERE id = ?1", nativeQuery = true)
+    @Query(value = "UPDATE tb_conta_cliente SET col_creditos_cliente = ?2 WHERE col_id_conta = ?1", nativeQuery = true)
     void alterarCredito(Long id, double novoCredito);
 
     @Transactional
     @Modifying
-    @Query(value = "UPDATE ContaCliente SET nome = ?2 WHERE id = ?1", nativeQuery = true)
+    @Query(value = "UPDATE tb_conta_cliente SET col_nome = ?2 WHERE col_id_conta = ?1", nativeQuery = true)
     void alterarNome(Long id, String novoNome);
+
+	@Query(value = "SELECT * FROM tb_conta_cliente WHERE col_id_conta = ?1", nativeQuery = true)
+    ContaCliente buscarPorId(Long id);
 
 }
