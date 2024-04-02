48cbdceb0818357737c01fc7dd37f59730540b16
CarlosEduardo-IFF <149334691+CarlosEduardo-IFF@users.noreply.github.com>
2024-04-01 22:10:43 -0300

    Atualizar o CarrinhoDeCompraRepository.java

--- E-commerce/src/main/java/repository/CarrinhoDeCompraRepository.java
+++ E-commerce/src/main/java/repository/CarrinhoDeCompraRepository.java
@@ -9,6 +9,6 @@ import br.edu.iff.ecommerce.model.CarrinhoDeCompra;
 @Repository
 public interface CarrinhoDeCompraRepository extends JpaRepository<CarrinhoDeCompra, Long> {
 
-    @Query(value = "SELECT * FROM CarrinhoDeCompra WHERE idCarrinho = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_carrinho_de_compra WHERE col_id_carrinho = ?1", nativeQuery = true)
     CarrinhoDeCompra buscarPorId(Long id);
 }
